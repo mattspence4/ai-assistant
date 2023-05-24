702f13e6e0bab782a9c632dc660fe283789456b2
mattspence4 <113352659+mattspence4@users.noreply.github.com>
2023-05-24 18:28:48 -0400

    First commit stated to add ui elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
+import {MainContainer, ChatContainer, MessageList, Message, MessageInput} from '@chatscope/chat-ui-kit-react'
 
 function App() {
+  const [isTyping, setIsTyping] = useState(false)
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, how can I help you today?",
+      sender: "ChatGPT"
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    };
+
+    const newMessages = [...messages, newMessage]
+
+    setMessages(newMessages);
+
+    // set typing indicator
+    setIsTyping(true)
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{position: "relative", height: "800px", width: "700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList 
+              typingIndicator={isTyping ? <typingIndicator content="generating response"/> : null}
+            >
+              {messages.map((message, i) =>{
+                return <Message key={i} model={message}/>
+              })}
+            </MessageList>
+            <MessageInput placeholder='Enter query here' onSend={handleSend}/>
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
