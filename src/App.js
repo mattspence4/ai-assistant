c739af41a3f77b2a1a4fe2adf24a387c1c7c7e24
mattspence4 <113352659+mattspence4@users.noreply.github.com>
2023-05-25 15:26:09 -0400

    completed post to openai api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
-import {MainContainer, ChatContainer, MessageList, Message, MessageInput} from '@chatscope/chat-ui-kit-react'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+const API_KEY = "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";
 
 function App() {
-  const [isTyping, setIsTyping] = useState(false)
+  const [isTyping, setIsTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
       message: "Hello, how can I help you today?",
-      sender: "ChatGPT"
+      sender: "ChatGPT",
     },
   ]);
 
@@ -17,30 +26,74 @@ function App() {
     const newMessage = {
       message: message,
       sender: "user",
-      direction: "outgoing"
+      direction: "outgoing",
     };
 
-    const newMessages = [...messages, newMessage]
+    const newMessages = [...messages, newMessage];
 
     setMessages(newMessages);
 
     // set typing indicator
-    setIsTyping(true)
+    setIsTyping(true);
+
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "respond like a personal assistant",
+    };
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        console.log(data.choices[0].message.content)
+      });
   }
- 
+
   return (
     <div className="App">
-      <div style={{position: "relative", height: "800px", width: "700px"}}>
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
         <MainContainer>
           <ChatContainer>
-            <MessageList 
-              typingIndicator={isTyping ? <typingIndicator content="generating response"/> : null}
+            <MessageList
+              typingIndicator={
+                isTyping ? (
+                  <TypingIndicator content="generating response" />
+                ) : null
+              }
             >
-              {messages.map((message, i) =>{
-                return <Message key={i} model={message}/>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />;
               })}
             </MessageList>
-            <MessageInput placeholder='Enter query here' onSend={handleSend}/>
+            <MessageInput placeholder="enter query here" onSend={handleSend} />
           </ChatContainer>
         </MainContainer>
       </div>
